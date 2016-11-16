d41c7d54f210423381a735d4f26b2880336f6b56
Dennis Schridde <devurandom@gmx.net>
2016-11-16 17:18:13 +0100

    Fix license to align with generated code
    Changed from EPL 1.0 to Apache License 2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject kubernetes "1.4.6-0.0.1"
+(defproject kubernetes "1.4.6-0.0.2"
   :description "Clojure API bindings for Kubernetes"
   :url "https://github.com/devurandom/clj-kubernetes"
-  :license {:name "Eclipse Public License v1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]])
--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ test -n "$pkgversion"
 test -f swagger-codegen-cli-$swgversion.jar || curl -O http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/$swgversion/swagger-codegen-cli-$swgversion.jar
 
 # Generate code
-java -jar swagger-codegen-cli-$swgversion.jar generate -i https://raw.githubusercontent.com/kubernetes/kubernetes/v$k8sversion/api/swagger-spec/v1.json -l clojure --additional-properties projectName=kubernetes,projectVersion=$k8sversion-$pkgversion,projectDescription="Clojure API bindings for Kubernetes",projectUrl=https://github.com/devurandom/clj-kubernetes,projectLicenseName="Eclipse Public License v1.0",projectLicenseUrl=http://www.eclipse.org/legal/epl-v10.html
+java -jar swagger-codegen-cli-$swgversion.jar generate -i https://raw.githubusercontent.com/kubernetes/kubernetes/v$k8sversion/api/swagger-spec/v1.json -l clojure --additional-properties projectName=kubernetes,projectVersion=$k8sversion-$pkgversion,projectDescription="Clojure API bindings for Kubernetes",projectUrl=https://github.com/devurandom/clj-kubernetes,projectLicenseName="Apache License, Version 2.0",projectLicenseUrl=http://www.apache.org/licenses/LICENSE-2.0.html
 
 # Fix code
 sed 's/apiv-/v1/' -i src/kubernetes/api/apiv_.clj
