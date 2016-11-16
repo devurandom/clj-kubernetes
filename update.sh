d64898fd12b33096f8c50732d95a363328aee170
Dennis Schridde <devurandom@gmx.net>
2016-11-16 16:42:14 +0100

    Add update script to download and generate the API bindings
    This is the only actual piece of code in this repo - everything else is auto-generated

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+swgversion=2.2.0
+
+k8sversion=$1
+test -n "$k8sversion"
+
+pkgversion=$2
+test -n "$pkgversion"
+
+# Download swagger-codegen
+test -f swagger-codegen-cli-$swgversion.jar || curl -O http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/$swgversion/swagger-codegen-cli-$swgversion.jar
+
+# Generate code
+java -jar swagger-codegen-cli-$swgversion.jar generate -i https://raw.githubusercontent.com/kubernetes/kubernetes/v$k8sversion/api/swagger-spec/v1.json -l clojure --additional-properties projectName=kubernetes,projectVersion=$k8sversion-$pkgversion,projectDescription="Clojure API bindings for Kubernetes",projectUrl=https://github.com/devurandom/clj-kubernetes,projectLicenseName="Eclipse Public License v1.0",projectLicenseUrl=http://www.eclipse.org/legal/epl-v10.html
+
+# Fix code
+sed 's/apiv-/v1/' -i src/kubernetes/api/apiv_.clj
+mv src/kubernetes/api/apiv_.clj src/kubernetes/api/v1.clj
+
+# Deploy to GitHub
+git add .
+git commit
+git tag v$k8sversion-$pkgversion
+git push --tags
+
+# Deploy to Clojars
+lein deploy clojars
